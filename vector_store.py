97e5d69bce744d56409f0785c1dbd2016690779d
Pritam Pani <112187958+pritampani@users.noreply.github.com>
2025-02-14 22:25:00 +0530

    vector_store.py

--- vector_store.py
+++ vector_store.py
@@ -1,8 +1,9 @@
 
-__import__('pysqlite3')
+import os
+os.environ["LD_LIBRARY_PATH"] = os.environ.get("LD_LIBRARY_PATH", "") + ":/usr/lib"
+import pysqlite3  # Force using correct SQLite
 import sys
-
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+sys.modules["sqlite3"] = pysqlite3
 import chromadb
 from sentence_transformers import SentenceTransformer
 
