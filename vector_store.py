025ebaa13e72e0160afe01cfaab8df937233b8eb
Pritam Pani <112187958+pritampani@users.noreply.github.com>
2025-02-14 13:32:56 +0530

    Update vector_store.py

--- vector_store.py
+++ vector_store.py
@@ -1,8 +1,7 @@
 import chromadb
 from sentence_transformers import SentenceTransformer
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import pysqlite3
+
 # Initialize ChromaDB client and collection
 client = chromadb.Client()
 collection_name = "knowledge_base"
