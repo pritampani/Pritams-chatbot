5eff6ac474018b5658c86e5fff07b05c6133db9c
Pritam Pani <112187958+pritampani@users.noreply.github.com>
2025-02-17 09:19:37 +0530

    vector_store.py

--- vector_store.py
+++ vector_store.py
@@ -1,8 +1,9 @@
-__import__('pysqlite3')
 import sys
-
-
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+try:
+    import pysqlite3
+    sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+except ImportError:
+    pass 
 import chromadb
 from sentence_transformers import SentenceTransformer
 
