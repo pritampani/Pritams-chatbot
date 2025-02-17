fce9dbcf9cc2dc2a55a1cf2f6cebde0d3bf290a0
Pritam Pani <112187958+pritampani@users.noreply.github.com>
2025-02-17 13:15:10 +0530

    main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import os
 import logging
 from vector_store import retrieve_context
 from pdf_processing import update_knowledge_base
-value = "AIzaSyAKHJmdAjCjVz38niGb3gSV91WcNmU5V1s"
+value=st.secrets["value"]
 
 # Configure logging
 logging.basicConfig(
