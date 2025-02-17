62f64b619f82b79d77ec0db78fe995f48179249d
Pritam Pani <112187958+pritampani@users.noreply.github.com>
2025-02-17 09:19:10 +0530

    Update pdf_processing.py

--- pdf_processing.py
+++ pdf_processing.py
@@ -1,7 +1,9 @@
-__import__('pysqlite3')
 import sys
-
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+try:
+    import pysqlite3
+    sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+except ImportError:
+    pass 
 import os
 import uuid
 from PyPDF2 import PdfReader
